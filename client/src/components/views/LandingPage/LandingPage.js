c24603c39a85a22017eaa84011e1de7068f07070
yeongbba <yeongmolee2@gmail.com>
2020-11-07 19:39:53 +0900

    make Landingpage1

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,16 +1,99 @@
-import React from 'react'
-import { FaCode } from "react-icons/fa";
+import React, { useEffect, useState } from "react";
+import Axios from "axios";
+import { Icon, Col, Card, Row } from "antd";
+import ImageSlider from "../../utils/ImageSlider";
+
+const { Meta } = Card;
 
 function LandingPage() {
+  const [Products, setProducts] = useState([]);
+  const [Skip, setSkip] = useState(0);
+  const [Limit, setLimit] = useState(8);
+  const [PostSize, setPostSize] = useState();
+
+  useEffect(() => {
+    const variables = {
+      skip: Skip,
+      limit: Limit,
+    };
+
+    getProducts(variables);
+  }, []);
+
+  const getProducts = (variables) => {
+    Axios.post("/api/product/getProducts", variables).then((response) => {
+      if (response.data.success) {
+        if (variables.loadMore) {
+          setProducts([...Products, ...response.data.products]);
+        } else {
+          setProducts(response.data.products);
+        }
+        setPostSize(response.data.postSize);
+      } else {
+        alert("Failed to fectch product datas");
+      }
+    });
+  };
+
+  const onLoadMore = () => {
+    let skip = Skip + Limit;
+
+    const variables = {
+      skip: skip,
+      limit: Limit,
+      loadMore: true,
+      filters: Filters,
+      searchTerm: SearchTerms,
+    };
+    getProducts(variables);
+    setSkip(skip);
+  };
+
+  const renderCards = Products.map((product, index) => {
     return (
-        <>
-            <div className="app">
-                <FaCode style={{ fontSize: '4rem' }} /><br />
-                <span style={{ fontSize: '2rem' }}>Let's Start Coding!</span>
-            </div>
-            <div style={{ float: 'right' }}>Thanks For Using This Boiler Plate by John Ahn</div>
-        </>
-    )
+      <Col lg={6} md={8} xs={24}>
+        <Card
+          hoverable={true}
+          cover={
+            <a href={`/product/${product._id}`}>
+              {" "}
+              <ImageSlider images={product.images} />
+            </a>
+          }
+        >
+          <Meta title={product.title} description={`$${product.price}`} />
+        </Card>
+      </Col>
+    );
+  });
+
+  return (
+    <div style={{ width: "75%", margin: "3rem auto" }}>
+      <div style={{ textAlign: "center" }}>
+        <h2>
+          {" "}
+          Let's Travel Anywhere <Icon type="rocket" />{" "}
+        </h2>
+      </div>
+
+      {/* Filter  */}
+
+      {/* Search  */}
+
+      <div>
+        <Row gutter={[16, 16]}>{renderCards}</Row>
+      </div>
+
+      <br />
+      <br />
+
+      {PostSize >= Limit && (
+        <div style={{ display: "flex", justifyContent: "center" }}>
+          <button onClick={onLoadMore}>Load More</button>
+        </div>
+      )}
+    </div>
+  );
 }
 
-export default LandingPage
+export default LandingPage;
--- server/routes/product.js
+++ server/routes/product.js
@@ -48,4 +48,20 @@ router.post("/uploadProduct", auth, (req, res) => {
   });
 });
 
+router.post("/getProducts", (req, res) => {
+  let limit = req.body.limit ? parseInt(req.body.limit) : 100;
+  let skip = parseInt(req.body.skip);
+
+  Product.find()
+    .populate("writer")
+    .skip(skip)
+    .limit(limit)
+    .exec((err, products) => {
+      if (err) return res.status(400).json({ success: false, err });
+      res
+        .status(200)
+        .json({ success: true, products, postSize: products.length });
+    });
+});
+
 module.exports = router;
