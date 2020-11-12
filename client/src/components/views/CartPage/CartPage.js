45f2e06acae3837086d543a8c4ad2a1aa2d2b446
yeongbba <yeongmolee2@gmail.com>
2020-11-13 00:02:02 +0900

    make cart detail page

--- client/src/components/views/CartPage/CartPage.js
+++ client/src/components/views/CartPage/CartPage.js
@@ -1,9 +1,12 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import { getCartItems } from "../../../_actions/user_actions";
+import UserCardBlock from "./Sections/UserCardBlock";
+import Axios from "axios";
 
 function CartPage(props) {
   const dispatch = useDispatch();
+  const [Total, setTotal] = useState(0);
 
   useEffect(() => {
     let cartItems = [];
@@ -23,9 +26,28 @@ function CartPage(props) {
     }
   }, [props.user.userData]);
 
+  const calculateTotal = (cartDetail) => {
+    let total = 0;
+
+    cartDetail.map((item) => {
+      total += parseInt(item.price, 10) * item.quantity;
+    });
+
+    setTotal(total);
+  };
+
   return (
     <div style={{ width: "85%", margin: "3rem auto" }}>
       <h1>My Cart</h1>
+      <div>
+        <UserCardBlock products={props.user.cartDetail} />
+
+        <div style={{ marginTop: "3rem" }}>
+          <h2>Total amount: ${Total} </h2>
+        </div>
+      </div>
+
+      {/* Paypal Button */}
     </div>
   );
 }
